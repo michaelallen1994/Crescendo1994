b3b800a4e4c46fa068293a86908ed5726d786950
michaelallen1994 <michael.allen@kckps.org>
2024-01-20 11:20:00 -0600

    Testbed files
    These are our working files that will eventually become the code for the season.

---
+++ RobotContainer.java
@@ -0,0 +1,81 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot;
+
+import frc.robot.subsystems.Climber;
+import frc.robot.subsystems.Drivetrain;
+import frc.robot.subsystems.Loader;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
+import edu.wpi.first.wpilibj2.command.button.JoystickButton;
+import edu.wpi.first.wpilibj2.command.button.Trigger;
+import frc.robot.Constants.OperatorConstants;
+import frc.robot.commands.Climb;
+import frc.robot.commands.Decend;
+import frc.robot.commands.Load;
+import frc.robot.commands.Unload;
+
+/**
+ * This class is where the bulk of the robot should be declared. Since Command-based is a
+ * "declarative" paradigm, very little robot logic should actually be handled in the {@link Robot}
+ * periodic methods (other than the scheduler calls). Instead, the structure of the robot (including
+ * subsystems, commands, and trigger mappings) should be declared here.
+ */
+public class RobotContainer {
+  // The robot's subsystems and commands are defined here...
+  private final Drivetrain driveTrain = new Drivetrain();
+  private final Loader loader = new Loader();
+  private final Climber climber = new Climber();
+
+  // Replace with CommandPS4Controller or CommandJoystick if needed
+  //private final CommandXboxController m_driverController =
+      //new CommandXboxController(OperatorConstants.kDriverControllerPort);
+
+  Joystick joystick = new Joystick(OperatorConstants.kDriverControllerPort);
+
+  /** The container for the robot. Contains subsystems, OI devices, and commands. */
+  public RobotContainer() {
+    // Configure the trigger bindings
+    driveTrain.setDefaultCommand(
+      Commands.run(() -> driveTrain.Drive(joystick.getX(), joystick.getY()), driveTrain));
+
+    configureBindings();  
+
+    new JoystickButton(joystick, 2).whileTrue((new Unload(loader)));
+    new JoystickButton(joystick, 1).whileTrue((new Load(loader)));
+    new JoystickButton(joystick, 8).whileTrue((new Climb(climber)));
+    new JoystickButton(joystick, 7).whileTrue((new Decend(climber)));
+
+  }
+
+  /**
+   * Use this method to define your trigger->command mappings. Triggers can be created via the
+   * {@link Trigger#Trigger(java.util.function.BooleanSupplier)} constructor with an arbitrary
+   * predicate, or via the named factories in {@link
+   * edu.wpi.first.wpilibj2.command.button.CommandGenericHID}'s subclasses for {@link
+   * CommandXboxController Xbox}/{@link edu.wpi.first.wpilibj2.command.button.CommandPS4Controller
+   * PS4} controllers or {@link edu.wpi.first.wpilibj2.command.button.CommandJoystick Flight
+   * joysticks}.
+   */
+  private void configureBindings() {
+    // Schedule `ExampleCommand` when `exampleCondition` changes to `true`
+    //new Trigger(m_exampleSubsystem::exampleCondition)
+        //.onTrue(new ExampleCommand(m_exampleSubsystem));
+
+    // Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
+    // cancelling on release.
+   // m_driverController.b().whileTrue(m_exampleSubsystem.exampleMethodCommand());
+  }
+
+  /**
+   * Use this to pass the autonomous command to the main {@link Robot} class.
+   *
+   * @return the command to run in autonomous
+   */
+  //public Command getAutonomousCommand() {
+    // An example command will be run in autonomous
+    //return Autos.exampleAuto(m_exampleSubsystem);
+  }
